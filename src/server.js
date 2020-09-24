5978e5d3ac42f6c107babf6de47aa2b5cbb6f2a0
Roberto Sousa <robertosousa186@gmail.com>
2020-09-23 21:43:22 -0300

    Criando servidor com express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on the port ${process.env.PORT}`);
+});
